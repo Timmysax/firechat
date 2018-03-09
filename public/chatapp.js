94e4e62eae424fdb6cb6f7484b28f5c163010a5e
PJ Essien <retrostereoyak@gmail.com>
2018-03-09 18:59:41 +0100

    Basic chat implementation (no auth)

--- public/chatapp.js
+++ public/chatapp.js
@@ -1,17 +1,19 @@
+// This function simply makes sure the browser/page is completely ready
+// before your code is executed
 function onReady(callback) {
 	(document.readyState === 'interactive' || document.readyState === 'complete') ? callback() : document.addEventListener('DOMContentLoaded', callback);
 };
 
+// App logic goes here
 function runChatApp() {
+    // References to the DOM nodes for important elements
     var messageList = document.getElementById('message-list');
     var chatForm = document.getElementById('chat-form');
     var usernameField = document.getElementById('username-field');
     var messageField = document.getElementById('message-field');
     
-    var DB = firebase.database();
-    var messagesRef = DB.ref('/messages');
-    
     function buildChatElement(chatObject) {
+        // Your code for rendering messages goes here
         var chatElement = document.createElement('li');
         var chatItem = document.createElement('div');
         var usernameElement = document.createElement('span');
@@ -30,7 +32,10 @@ function runChatApp() {
 
         return chatElement;
     }
-    
+
+    // Setup firebase database here
+    var DB = firebase.database();
+    var messagesRef = DB.ref('/messages');
     messagesRef.on('child_added', function(snapshot) {
         var chatObject = snapshot.val();
         var chatElement = buildChatElement(chatObject);
@@ -39,8 +44,11 @@ function runChatApp() {
     });
     
     chatForm.addEventListener('submit', function(event) {
+        // This prevents the default submit behaviour so you can completely
+        // control the submission
         event.preventDefault();
-    
+
+        // Your code for submitting messages goes here
         var username = usernameField.value.trim();
         var message = messageField.value;
         if (username && message) {
@@ -53,9 +61,11 @@ function runChatApp() {
         usernameField.value = username;
         usernameField.setAttribute('disabled', true);
         messageField.value = '';
-    
+
+        // This stops the page from reloading on submit
         return false;
     });
 }    
 
+// Start app on ready!
 onReady(runChatApp);
