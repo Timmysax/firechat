4402a638fd64648e77d9734b66e716dd96098f49
PJ Essien <retrostereoyak@gmail.com>
2018-03-10 09:50:03 +0100

    Fix version history

--- public/chatapp.js
+++ public/chatapp.js
@@ -7,9 +7,30 @@ function onReady(callback) {
 // App logic goes here
 function runChatApp() {
     // Set up references to the DOM nodes for important elements
+    var messageList = document.getElementById('message-list');
+    var chatForm = document.getElementById('chat-form');
+    var usernameField = document.getElementById('username-field');
+    var messageField = document.getElementById('message-field');
     
     function buildChatElement(chatObject) {
         // Your code for rendering messages goes here
+        var chatElement = document.createElement('li');
+        var chatItem = document.createElement('div');
+        var usernameElement = document.createElement('span');
+        var messageElement = document.createElement('span');
+    
+        chatItem.className = 'chat-item';
+        usernameElement.className = 'username';
+        messageElement.className = 'message';
+
+        usernameElement.innerText = chatObject.username + ': ';
+        messageElement.innerText = chatObject.message;
+    
+        chatItem.appendChild(usernameElement);
+        chatItem.appendChild(messageElement);
+        chatElement.appendChild(chatItem);
+
+        return chatElement;
     }
 
     function submitHandler(event) {
@@ -18,14 +39,36 @@ function runChatApp() {
         event.preventDefault();
 
         // Your code for submitting messages goes here
+        var username = usernameField.value.trim();
+        var message = messageField.value;
+        if (username && message) {
+            messagesRef.push({
+                username: username,
+                message: message
+            });
+        }
+
+        usernameField.value = username;
+        usernameField.setAttribute('disabled', true);
+        messageField.value = '';
 
         // This stops the page from reloading on submit
         return false;
     }
 
     // Setup firebase database here
+    var DB = firebase.database();
+    var messagesRef = DB.ref('/messages');
+
+    messagesRef.on('child_added', function(snapshot) {
+        var chatObject = snapshot.val();
+        var chatElement = buildChatElement(chatObject);
+        messageList.appendChild(chatElement);
+        messageList.scrollTop = messageList.scrollHeight;
+    });
     
     // Setup submit event handler here
+    chatForm.addEventListener('submit', submitHandler);
 }    
 
 // Start app on ready!
